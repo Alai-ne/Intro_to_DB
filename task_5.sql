7068182a8def604f378bdcf0866bc6908c8aca30
Alai-ne <alaineakeza12@gmail.com>
2026-01-03 07:19:15 -0800

    save

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,1 @@
-INSERT INTO Customer (Customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT IGNORE INTO Customer (Customer_id, customer_name, email, address)  VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
